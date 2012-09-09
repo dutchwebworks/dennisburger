9a5f8060017e750b2c2d98c2f87829866135cbfe
Dennis Burger <dennis@dutchwebworks.nl>
2012-09-09 15:22:33 +0200

    Added enquire.js example. Updated the javascripts to use enquire.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,20 @@
-// CSS3 MediaQueries breakpoints
+// MediaQuery breakpoints
 var mqbreakpoint01 = 500;
 
-// Functions
-var Utils = {
-	q : function(q, res) {
-		if (document.querySelectorAll) {
-			res = document.querySelectorAll(q);
-		} else {
-			var d = document,
-			a = d.styleSheets[0] || d.createStyleSheet();
-			a.addRule(q,'f:b');
-			for(var l=d.all,b=0,c=[],f=l.length;b<f;b++)
-			l[b].currentStyle.f && c.push(l[b]);
-
-			a.removeRule(0);
-			res = c;
-		}
-		return res;
-	},
-}
-
-// Document load
-$(document).ready(function() {
-	// When the window resizes
-	$(window).resize(function() {
-		// Check CSS3 MediaQuery, and remove inline style
-		// this re-enables the menu when resizing the webbrowser manually
-		if(Modernizr.mq('(min-width: ' + mqbreakpoint01 + 'px)')) {
-			$('#navigation').removeAttr('style');
-		}
-	});
-
-	// show menu on smaller screens
-	$('#showMenu').click(function() {
-		$('#navigation').toggle();
-	});
-
-	/*
-	$("#showMenu").toggle(
-		function () {
-			$('#navigation').slideDown();
-		},
-		function () {
-			$('#navigation').slideUp('fast');
-		}
-	);
-	*/	
-});	
+enquire.register("(max-width:" + mqbreakpoint01 + "px)", {
+	// Narrower than breakpoint
+    match : function() {
+		$('body').removeClass('largeScreen').toggleClass('smallScreen');
+    },
+    // Wider than breakpoint
+    unmatch : function() {
+    	$('body').removeClass('smallScreen').toggleClass('largeScreen');
+    	$('#navigation').removeAttr('style');
+    },
+    // Fire once when ready
+    setup : function() {
+		$('#showMenu').click(function() {
+			$('#navigation').toggle();
+		});
+    }
+}).listen();
