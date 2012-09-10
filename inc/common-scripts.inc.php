b91afb682e01516358e65268b17d387f444484d7
Dennis Burger <dennis@dutchwebworks.nl>
2012-09-10 12:54:53 +0200

    Added fix for Win/IE CSS gradient.

--- inc/common-scripts.inc.php
+++ inc/common-scripts.inc.php
@@ -1,7 +1,7 @@
 <!-- <script src="/js/libs/zepto.1.0.min.js"></script> -->
 
 <!-- Grab Google CDN's jQuery. fall back to local if necessary -->
- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
 <script>!window.jQuery && document.write(unescape('%3Cscript src="/js/libs/jquery.1.8.1.min.js"%3E%3C/script%3E'))</script>
 
 
