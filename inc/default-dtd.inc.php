798552925ed078c505bcc7baca6c3556ff8a36b6
Dennis Burger <dennis@dutchwebworks.nl>
2012-12-22 11:57:52 +0100

    Removed the Win/IE HTML-tag conditional tags in favor of Conditionizr.

--- inc/default-dtd.inc.php
+++ inc/default-dtd.inc.php
@@ -1,7 +1,2 @@
 <!doctype html>
-<!--[if IE 6]><html class="no-js ie lte9 lte8 lte7 lte6 ie6"><![endif]-->
-<!--[if IE 7]><html class="no-js ie lte9 lte8 lte7 ie7"><![endif]-->
-<!--[if IE 8]><html class="no-js ie lte9 lte8 ie8"><![endif]-->
-<!--[if IE 9]><html class="no-js ie lte9 ie9"><![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--><html class="no-js"><!--<![endif]-->
-<html class="no-js" lang="en">
+<html class="no-js" lang="nl">
